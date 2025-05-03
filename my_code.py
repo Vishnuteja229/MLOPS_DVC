f4d9c3156518e00c5678a9c9f92243c96d566c73
Vishnuteja229 <vishnutejakotagiri22@gmail.com>
2025-05-03 23:36:09 +0530

    second version

--- my_code.py
+++ my_code.py
@@ -13,6 +13,9 @@ df.loc[len(df.index)]=new_row
 new_row_2={'Name': 'GF2', 'Age': 30, 'City': 'City2'}
 df.loc[len(df.index)]=new_row_2
 
+new_row_3={'Name': 'GF3', 'Age': 15, 'City': 'hyd'}
+df.loc[len(df.index)]=new_row_3
+
 data_dir='data'
 os.makedirs(data_dir,exist_ok=True)
 
